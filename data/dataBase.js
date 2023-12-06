cc89108263306446db482b2f210e6e4f36f65ddb
Aditya <aditya.gaonkar1907@gmail.com>
2023-12-06 17:34:48 +0530

    DataBase Changes

--- data/dataBase.js
+++ data/dataBase.js
@@ -4,4 +4,4 @@ import mongoose from "mongoose";
 export const connectDB = async ()=>{
     await mongoose.connect(process.env.MONGO_URI,{dbName:"backendAPI"});
 }
-connectDB().then(()=>console.log("dataBase connected!")).catch(err => console.log(err));
+connectDB().then((c)=>console.log("dataBase connected!")).catch(err => console.log(err));
